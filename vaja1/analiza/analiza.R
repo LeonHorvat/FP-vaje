6590768be61f2282d95b8c1554ef9dd726a76996
Leon Horvat <leon.horvat1@gmail.com>
2017-10-20 16:59:04 +0200

    začetek analize

--- vaja1/analiza/analiza.R
+++ vaja1/analiza/analiza.R
@@ -1,5 +1,17 @@
+#1.(c)
+
 casovna_vrsta1 <- ts(euribor[,6], start=c(2011,1), frequency=12)
 casovna_vrsta2 <- ts(euribor[,7], start=c(2011,1), frequency=12)
 
-ts.plot(casovna_vrsta1, casovna_vrsta2, xlab='leto', ylab ='obrestna mera', main = '6-mesečna in 9-mesečna obrestna', col=c('blue','green'), lwd = 3)
-legend()
+ts.plot(casovna_vrsta1, casovna_vrsta2, xlab='leto', ylab ='obrestna mera', main = '6-mesečna in 9-mesečna obrestna', col=c('blue','green'), lwd = 2)
+legend("topright", legend=c('6m euribor', '9m euribor'), col=c('blue','green'),lty=1:1, cex=0.8)
+
+#2.
+
+#3.
+#(a)
+
+term_obr_mere <- subset(euribor, select=c('6m','9m'))
+class(term_obr_mere) <- 'numeric'
+class(term_obr_mere) <- 'data.frame'
+term_obr_mere$terminska <- NULL 
--- vaja1/uvoz/uvoz.R
+++ vaja1/uvoz/uvoz.R
@@ -1,4 +1,19 @@
-euribor2011 <- read_csv('podatki/hist_EURIBOR_2011.csv') %>%
+euribor2011 <- read_csv('vaja1/podatki/hist_EURIBOR_2011.csv') %>%
   subset(select=c('X1','03/01/2011', '01/02/2011', '01/03/2011','01/04/2011','02/05/2011','01/06/2011','01/07/2011','01/08/2011','01/09/2011','03/10/2011','01/11/2011','01/12/2011'))%>%
   t()
-imena <- 
+
+imena <- as.character(unlist(euribor2011[1,]))
+euribor2011 <- euribor2011[-1,]
+colnames(euribor2011) <- imena
+
+euribor2012 <- read_csv('vaja1/podatki/hist_EURIBOR_2012.csv') %>%
+  subset(select=c('X1','02/01/2012', '01/02/2012', '01/03/2012','02/04/2012','02/05/2012','01/06/2012','02/07/2012','01/08/2012','03/09/2012','01/10/2012','01/11/2012','03/12/2012'))%>%
+  t()
+euribor2012 <- euribor2012[-1,]
+euribor2013 <- read_csv('vaja1/podatki/hist_EURIBOR_2013.csv') %>%
+  subset(select=c('X1','02/01/2013', '01/02/2013', '01/03/2013','02/04/2013','02/05/2013','03/06/2013','01/07/2013','01/08/2013','02/09/2013','01/10/2013','01/11/2013','02/12/2013'))%>%
+  t()
+euribor2013 <- euribor2013[-1,]
+
+euribor <- rbind(euribor2011, euribor2012, euribor2013) %>%
+  subset(select=c(1,2,4,5,6,9,12,15))
